c66cfeaf3238ae4c26a2e02971cc57d7019764b5
Julien <j.jacquemet@see.fr>
2021-01-28 23:51:53 +0100

    fait marcher le bluetooth

--- app/src/main/java/fr/julienj/myapplication/MainActivity.java
+++ app/src/main/java/fr/julienj/myapplication/MainActivity.java
@@ -1,6 +1,13 @@
 package fr.julienj.myapplication;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
@@ -14,12 +21,17 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Environment;
+import android.os.ParcelUuid;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.Formatter;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import com.corundumstudio.socketio.AckRequest;
@@ -38,23 +50,51 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+
+import me.aflak.bluetooth.Bluetooth;
+import me.aflak.bluetooth.interfaces.BluetoothCallback;
+import me.aflak.bluetooth.interfaces.DeviceCallback;
+import me.aflak.bluetooth.interfaces.DiscoveryCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String url = "http://127.0.0.1:9000/index.html";
     private Configuration config;
     private SocketIOServer server;
+
+    private Bluetooth bluetooth;
+    private ArrayAdapter<String> scanListAdapter;
+    private ArrayAdapter<String> pairedListAdapter;
+    private List<BluetoothDevice> pairedDevices;
+    private List<BluetoothDevice> scannedDevices;
+
+    private boolean scanning = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
 
         if (Build.VERSION.SDK_INT >= 23) {
             int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
             if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (getApplicationContext().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 1);
+
             }
         }
 
@@ -87,31 +127,145 @@ public class MainActivity extends AppCompatActivity {
 
 
         System.out.println("jj "+ip);
+
+        ChatServer s = null;
+        try {
+            s = new ChatServer(9091);
+            s.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        /*String[] names = new String[]{"Samsung Galaxy S7"};
+        List<ScanFilter> filters = null;
+        if(names != null) {
+            filters = new ArrayList<>();
+            for (String name : names) {
+                ScanFilter filter = new ScanFilter.Builder()
+                        .setDeviceName(name)
+                        .build();
+                filters.add(filter);
+            }
+        }*/
+
+        String[] peripheralAddresses = new String[]{"C4:BE:84:1A:C2:07"};
+// Build filters list
+        List<ScanFilter> filters = null;
+        if (peripheralAddresses != null) {
+            filters = new ArrayList<>();
+            for (String address : peripheralAddresses) {
+                ScanFilter filter = new ScanFilter.Builder()
+                        .setDeviceAddress(address)
+                        .build();
+                filters.add(filter);
+            }
+        }
+
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothLeScanner scanner = adapter.getBluetoothLeScanner();
+
+         ScanCallback scanCallback = new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+                BluetoothDevice device = result.getDevice();
+                // ...do whatever you want with this found device
+                System.out.println("jj "+device.getAddress());
+            }
+
+            @Override
+            public void onBatchScanResults(List<ScanResult> results) {
+                // Ignore for now
+                System.out.println("jj fail2");
+            }
+
+            @Override
+            public void onScanFailed(int errorCode) {
+                // Ignore for now
+                System.out.println("jj fail1");
+            }
+        };
+
+        ScanSettings scanSettings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+                .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
+                .setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE)
+                .setNumOfMatches(ScanSettings.MATCH_NUM_ONE_ADVERTISEMENT)
+                .setReportDelay(0L)
+                .build();
+
+        if (scanner != null) {
+            scanner.startScan(filters, scanSettings, scanCallback);
+            Log.d("test", "scan started");
+            System.out.println("jj fail3");
+        }  else {
+            Log.e("test", "could not get scanner object");
+            System.out.println("jj fail4");
+        }
+
+
+        bluetooth = new Bluetooth(getApplicationContext());
+        //bluetooth.setCallbackOnUI(this);
+        bluetooth.setBluetoothCallback(bluetoothCallback);
+        bluetooth.setDiscoveryCallback(discoveryCallback);
+        bluetooth.onStart();
+        System.out.println("jj " +bluetooth.getPairedDevices());
+
+        bluetooth.connectToAddress("2C:33:7A:26:40:A6");
+
+
+        bluetooth.startScanning();
+
+        bluetooth.setDeviceCallback(new DeviceCallback() {
+            @Override public void onDeviceConnected(BluetoothDevice device) {}
+            @Override public void onDeviceDisconnected(BluetoothDevice device, String message) {}
+            @Override public void onMessage(byte[] message) {
+                String mess="";
+                for(int i=0 ; i<message.length; i++){
+                    mess+= String.valueOf((char)message[i]);
+                }
+                System.out.println("jj "+mess);
+                bluetooth.send(mess+"\r\n");
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+
+            @Override public void onConnectError(BluetoothDevice device, String message) {}
+        });
+
+        /*
+
         config=new Configuration();
-        config.setHostname("127.0.0.1");
+        config.setHostname("0.0.0.0");
         config.setPort(9092);
+        config.setOrigin("Access-Control-Allow-Origin");
+
 
         server=new SocketIOServer(config);
         server.start();
         ConnectListener tt=new ConnectListener() {
             @Override
             public void onConnect(SocketIOClient client) {
+                System.out.println("jj connect");
 
+                System.out.println("jj "+config.getOrigin());
             }
         };
 
         DataListener t= new DataListener() {
             @Override
             public void onData(SocketIOClient client, Object data, AckRequest ackSender) throws Exception {
-
+                System.out.println("jj data");
             }
         };
 
 
 
         server.addConnectListener(tt);
-        server.addEventListener("ee", this.getClass(), t);
-
+        server.addEventListener("message", this.getClass(), t);
+        */
 
 
 
@@ -149,5 +303,73 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
+
+    private BluetoothCallback bluetoothCallback = new BluetoothCallback() {
+        @Override
+        public void onBluetoothTurningOn() {
+        }
+
+        @Override
+        public void onBluetoothOn() {
+            System.out.println("jj 2");
+            pairedDevices = bluetooth.getPairedDevices();
+            for(BluetoothDevice device : pairedDevices){
+                System.out.println("jj 2"+device.getAddress()+" : "+device.getName());
+            }
+        }
+
+        @Override
+        public void onBluetoothTurningOff() {
+            //scanButton.setEnabled(false);
+        }
+
+        @Override
+        public void onBluetoothOff() {
+        }
+
+        @Override
+        public void onUserDeniedActivation() {
+
+        }
+    };
+
+    private DiscoveryCallback discoveryCallback = new DiscoveryCallback() {
+        @Override
+        public void onDiscoveryStarted() {
+            System.out.println("jj 1");
+            scannedDevices = new ArrayList<>();
+            scanning = true;
+        }
+
+        @Override
+        public void onDiscoveryFinished() {
+            System.out.println("jj 3");
+            scanning = false;
+        }
+
+        @Override
+        public void onDeviceFound(BluetoothDevice device) {
+            System.out.println("jj 4" +device.getAddress()+" : "+device.getName());
+            scannedDevices.add(device);
+            scanListAdapter.add(device.getAddress()+" : "+device.getName());
+        }
+
+        @Override
+        public void onDevicePaired(BluetoothDevice device) {
+
+        }
+
+        @Override
+        public void onDeviceUnpaired(BluetoothDevice device) {
+
+        }
+
+        @Override
+        public void onError(int errorCode) {
+
+        }
+    };
 }
